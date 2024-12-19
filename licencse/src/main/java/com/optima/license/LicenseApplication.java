d0364773d5381010aa1bf5dd465249506da22162
Amit Maindola <maindola.amit@gmail.com>
2024-12-19 10:15:10 +0530

    tmp

--- licencse/src/main/java/com/optima/license/LicenseApplication.java
+++ licencse/src/main/java/com/optima/license/LicenseApplication.java
@@ -1,15 +1,21 @@
 package com.optima.license;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
+@Slf4j
 public class LicenseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LicenseApplication.class, args);
+        val context = SpringApplication.run(LicenseApplication.class, args);
+        String dfInfo = context.getEnvironment().getProperty("spring.datasource.username")
+                + "@" + context.getEnvironment().getProperty("spring.datasource.url");
+        log.info("DB connection : {}", dfInfo);
     }
 
 }
